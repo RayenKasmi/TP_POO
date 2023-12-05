6eaac90b9828937d22171326d196282741ca9202
RayenKasmi <146983595+RayenKasmi@users.noreply.github.com>
2023-12-05 12:10:59 +0100

    Delete Course.java

--- Course.java
+++
@@ -1,9 +0,0 @@
-public class Course {
-    protected int courseId;
-    protected String courseName;
-    protected Instructor instructor;
-    public int getCourseId(){return(this.courseId);}
-    public String getCourseName(){return (courseName);}
-    public Instructor getInstructor(){return(instructor);}
-
-}
