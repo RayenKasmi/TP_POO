31debbe91956125fdd744db47033c61f8e28ca86
RayenKasmi <146983595+RayenKasmi@users.noreply.github.com>
2023-12-05 12:10:23 +0100

    Delete Instructor.java

--- Instructor.java
+++
@@ -1,9 +0,0 @@
-public class Instructor {
-    protected int instructorId;
-    protected String firstName;
-    protected String lastName;
-    public int getInstructorId(){return(this.instructorId);}
-    public String getFirstName(){return (this.firstName);}
-    public String getLastName(){return(this.lastName);}
-
-}
