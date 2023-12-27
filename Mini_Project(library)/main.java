9df379c7967a8f0918141fdc2f4cb1a44d81b9e4
RayenKasmi <146983595+RayenKasmi@users.noreply.github.com>
2023-12-27 02:03:53 +0100

    Create main.java

---
+++ Mini_Project(library)/main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*****Welcome to the Library*****");
+        Library library=new Library();
+        library.menu.LibraryApplication(library);
+    }
+}
